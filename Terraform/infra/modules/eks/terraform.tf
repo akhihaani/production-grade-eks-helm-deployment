bbf25d95443e0a2b07a4f666e761cf1afb69eed5
akhihaani <akhihaani@gmail.com>
2026-09-09 18:06:02 +0100

    chore: pin AWS provider to 6.63.0 across modules

--- Terraform/infra/modules/eks/terraform.tf
+++ Terraform/infra/modules/eks/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.11"
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 6.0" }
+    aws = { source = "hashicorp/aws", version = "~> 6.63.0" }
     tls = { source = "hashicorp/tls", version = "~> 4.0" }
   }
 }
